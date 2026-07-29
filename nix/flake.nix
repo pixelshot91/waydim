dc3d4055263837814a7c5cb6244dd3dbf1563f32
Julien Gautier <gautier.julien@gmail.com>
2026-07-29 23:35:34 +0200

    Use latest toolchain, add Cargo.lock, un-ignore it, fix flake,nix

--- nix/flake.nix
+++
@@ -1,7 +0,0 @@
-{
-  description = "waydim - hybrid hardware + software display dimmer (Wayland compatible)",
-  inputs = {},
-  outputs = { self, ... }: {
-    packages.x86_64-linux.default = pkgs: pkgs.rustPackages.rustc;
-  }
-}
